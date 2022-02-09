b2e0e1f533b3726e9e658aaf54675d94edde233a
doingz <allard@doingz.com>
2022-02-09 20:56:59 +0100

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -3,13 +3,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts")
   eleventyConfig.addPassthroughCopy("js")
   eleventyConfig.addPassthroughCopy("uploads")
-
-
-  eleventyConfig.addFilter('markdown', function(value) {
-      let markdown = require('markdown-it')({
-          html: false
-      });
-      return markdown.render(value);
-  });
-
+  eleventyConfig.addPassthroughCopy("admin")
 };
