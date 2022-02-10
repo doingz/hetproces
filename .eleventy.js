f8930c811f81efed7447d25a42bb31befca37800
doingz <allard@doingz.com>
2022-02-10 16:37:33 +0100

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("js")
   eleventyConfig.addPassthroughCopy("uploads")
   eleventyConfig.addPassthroughCopy("admin")
+
+  eleventyConfig.setLiquidOptions({
+    dynamicPartials: false,
+    strictFilters: false, // renamed from `strict_filters` in Eleventy 1.0
+  });
+
 };
